f423f130b35133e1e9df15c9b77789dc44d4ec3c
Eric D. Torres <etorresnotary@gmail.com>
2020-09-03 18:13:12 -0500

    exploring fetch API

--- app.js
+++ app.js
@@ -1,2 +1,10 @@
 //ExchangeRate-API
 //https://www.exchangerate-api.com/
+
+function calculate() {
+  fetch("items.json")
+  .then(res => console.log(res))
+  .then(date => console.log(data))
+}
+
+calculate();
